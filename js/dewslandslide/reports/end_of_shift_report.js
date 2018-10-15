4d4eefe2a50dbef047dc08c070960b72a61301f6
jhndvdgvr <david063095@gmail.com>
2018-10-15 08:51:55 +0800

    added .dost in hostname checker

--- js/dewslandslide/reports/end_of_shift_report.js
+++ js/dewslandslide/reports/end_of_shift_report.js
@@ -340,7 +340,7 @@ function initializeMailRecipients () {
     $("#mail_recipients_row .bootstrap-tagsinput").css("width", "100%");
     const $recipients = $("#recipients_span");
     const emails = [];
-    if (HOSTNAME.includes("dynaslope.phivolcs.gov.ph")) {
+    if (HOSTNAME.includes("dynaslope.phivolcs.dost.gov.ph")) {
         emails.push("rusolidum@phivolcs.dost.gov.ph", "asdaag48@gmail.com", "phivolcs-senslope@googlegroups.com", "phivolcs-dynaslope@googlegroups.com");
     } else if ($recipients.html().length === 0) {
         emails.push("dynaslope.mail@gmail.com");
@@ -945,7 +945,7 @@ function sendReport (site_code, event_id) {
     const recipients = $("#recipients").tagsinput("items");
     const form_data = new FormData();
     const file_data = typeof upload_files[site_code] !== "undefined" ? upload_files[site_code] : [];
-    const is_test = (HOSTNAME.contains("dynaslope.phivolcs.gov.ph")) ? false : true;
+    const is_test = (HOSTNAME.contains("dynaslope.phivolcs.dost.gov.ph")) ? false : true;
     const form = {
         event_id,
         site_code,
