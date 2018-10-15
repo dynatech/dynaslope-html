a40dea90d6999df44296637c453a21a9270eb7c8
John Geliberte <geliberteofficials@gmail.com>
2018-10-15 11:36:49 +0800

    Finalize no ack bug fix

--- js/dewslandslide/communications_beta/cbx_dashboard.js
+++ js/dewslandslide/communications_beta/cbx_dashboard.js
@@ -2,6 +2,7 @@ let ewi_event_id = null;
 let ewi_site_id = null;
 let ewi_timestamp = null;
 let ewi_data_timestamp = null;
+let ewi_event_start = null;
 let temp_ewi_button_container = null;
 $(document).ready(() => {
 	let temp_ewi_template_holder = "";
@@ -25,6 +26,7 @@ function initializeEwiPhoneButton() {
         ewi_timestamp = moment(current_row.data_timestamp).add(30, "m").format('h:mm A');
         ewi_event_id = current_row.event_id;
         ewi_site_id = current_row.site_id;
+        ewi_event_start = current_row.event_start;
         ewi_data_timestamp = moment(current_row.data_timestamp).add(30, "m").format('YYYY-MM-DD H:mm:ss');
         let request = {
         	"type": "getEwiDetailsViaDashboard",
@@ -102,6 +104,7 @@ function initializeSendButton() {
 			"site_id": ewi_site_id,
 			"timestamp": ewi_timestamp,
 			"data_timestamp":ewi_data_timestamp,
+            "event_start": ewi_event_start,
 			"previous_release_time": ewi_timestamp = moment(ewi_data_timestamp).subtract(240, "m").format('h:mm A'),
 			"recipients": $("#ewi-recipients-dashboard").tagsinput('items'),
 			"msg": $("#constructed-ewi-amd").val() + dashboard_sms_signature,
