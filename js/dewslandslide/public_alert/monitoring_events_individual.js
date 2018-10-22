9fb6fa81f3bf4212c66412096a14b1d1c32c9551
Kevin Dhale dela Cruz <kevindhaledelacruz@gmail.com>
2018-10-22 16:27:38 +0800

    - bug fix on no eos

--- js/dewslandslide/public_alert/monitoring_events_individual.js
+++ js/dewslandslide/public_alert/monitoring_events_individual.js
@@ -81,7 +81,7 @@ $(document).ready(() => {
 
     $.when(getDataForEWICard(EVENT_ID), getEventNarratives(EVENT_ID), getEventEOSAnalysis(EVENT_ID))
     .done((ewi_data, [event_narratives], [eos]) => {
-        if (STATUS === "routine") eos = [];
+        if (STATUS === "routine" || typeof eos.analysis === "undefined") eos = [];
         const timeline_array = compileTimelineCardDataIntoArray(ewi_data, event_narratives, eos);
 
         timeline_array.sort((a, b) => moment(b.ts).diff(a.ts));
