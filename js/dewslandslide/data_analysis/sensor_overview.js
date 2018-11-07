26c8aabc273669a536e7d3b5e5833cb26c4da8cd
raybuensuceso <rayandrew.buensuceso>
2018-11-07 16:54:45 +0800

    + created JS file for plotting data tables of sensor overview page

--- js/dewslandslide/data_analysis/sensor_overview.js
+++ js/dewslandslide/data_analysis/sensor_overview.js
@@ -1,43 +1,109 @@
-$(document).ready(function(e) {
-	
-	d3.select(window).on("resize", resize2)
-	function resize2() {
-		d3.selectAll("#svg-alert").remove();
-		d3.selectAll("#svg-presence").remove();
-		svg.selectAll(".dot").remove();
-		svg.selectAll(".dot1").remove();
-		svg.selectAll(".dot2").remove();
-		svg.selectAll(".line").remove();
-		svg.selectAll(".tick").remove();
-		svg.selectAll(".axislabel").remove();
-		initAlertPlot();
-		dataPresencePlot();
-	}
-	$(document).ready(function(){
-		$('[data-toggle="tooltip"]').tooltip();   
-	});
 
-
-	$.get("../api/AllSiteDetails").done(function(data){
-		var all_sites_details = JSON.parse(data);
-		var map_pins=[]
-		for (i = 0; i < all_sites_details.length; i++) {
-			map_pins.push([parseFloat(all_sites_details[i].lat) ,parseFloat(all_sites_details[i].lon),all_sites_details[i].name])
-		}
-		initMap(map_pins)
-	})
-	function initMap(map_pins) {
-		var map = new google.maps.Map(document.getElementById('map-canvas'), {
-			zoom: 6,
-			center: new google.maps.LatLng(11.8978,121.9094),
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		});
-
-		for (a = 0; a < map_pins.length; a++) {
-			var marker = new google.maps.Marker({
-				position: new google.maps.LatLng( map_pins[a][0],map_pins[a][1]),
-				map: map
-			});
-		}
-	}
+$(document).ready(() => {
+    generateDataTables();
 });
+
+function deactivatedSensors () {
+    return $.getJSON("../sensor_overview/deactivatedSensors")
+    .catch((error) => {
+        console.error(error);
+    });
+}
+
+function deactivatedLoggers () {
+    return $.getJSON("../sensor_overview/deactivatedLoggers")
+    .catch((error) => {
+        console.error(error);
+    });
+}
+
+function loggerHealth () {
+    return $.getJSON("../sensor_overview/loggerHealth")
+    .catch((error) => {
+        console.error(error);
+    });
+}
+
+function rainHealth () {
+    return $.getJSON("../sensor_overview/rainHealth")
+    .catch((error) => {
+        console.error(error);
+    });
+}
+
+function somsHealth () {
+    return $.getJSON("../sensor_overview/somsHealth")
+    .catch((error) => {
+        console.error(error);
+    });
+}
+
+function piezoHealth () {
+    return $.getJSON("../sensor_overview/piezoHealth")
+    .catch((error) => {
+        console.error(error);
+    });
+}
+
+function generateDataTables () {
+    const sensor_overview = [
+        deactivatedSensors(),
+        deactivatedLoggers(),
+        loggerHealth(),
+        rainHealth(),
+        somsHealth(),
+        piezoHealth()
+    ];
+
+    sensor_overview.forEach((data) => {
+        data.done((value) => {
+            value.forEach((val) => {
+                createTables(val[0], val[1]);
+            });
+        });
+    });
+}
+
+function createTables (data_source, table_id) {
+    const table_container = $("#table-container").clone().removeAttr("id").prop("hidden", false);
+    const table = $("#table-div").clone();
+    const table_template = table.find("#table-template").clone().prop("hidden", false);
+
+    if (table_id === "#deact-sensor" || table_id === "#deact-logger") {
+        data_source.forEach((info) => {
+            table_template.find(".removable").remove();
+            table_template.find("#cheader").text("Date Deactivated");
+
+            const table_row = table_template
+            .find("#deact-row-template")
+            .clone()
+            .removeAttr("id")
+            .prop("hidden", false);
+
+            table_row.find("#logger-name").text(info.logger_name);
+            table_row.find("#date-deactivated").text(info.date_deactivated);
+            table_template.append(table_row);
+        });
+    } else {
+        data_source.forEach((info) => {
+            const table_row = table_template.find("#row-template").clone().removeAttr("id").prop("hidden", false);
+            const {
+                logger_name, time_delta, data_presence, last_data
+            } = info;
+            const lookup = [
+                ["#logger-name", logger_name],
+                ["#time-delta", time_delta],
+                ["#data-presence", data_presence],
+                ["#last-data-available", last_data]
+            ];
+            lookup.forEach(([element, text]) => {
+                table_row.find(element).text(text);
+            });
+            table_template.append(table_row);
+        });
+    }
+    table.append(table_template);
+    table_container.append(table);
+    $(`${table_id}`).append(table_container);
+    table_template.DataTable();
+}
