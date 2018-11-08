fdc537debbac5d61cf0c82dc20b581c4d3120f59
raybuensuceso <rayandrew.buensuceso>
2018-11-09 05:45:11 +0800

    - modified the js file for the table revisions of deactivated sensors and loggers.

--- js/dewslandslide/data_analysis/sensor_overview.js
+++ js/dewslandslide/data_analysis/sensor_overview.js
@@ -1,4 +1,8 @@
 
+let table_container;
+let table;
+let table_template;
+
 $(document).ready(() => {
     generateDataTables();
 });
@@ -46,56 +50,102 @@ function piezoHealth () {
 }
 
 function generateDataTables () {
-    const sensor_overview = [
-        deactivatedSensors(),
-        deactivatedLoggers(),
+    const active_loggers = [
         loggerHealth(),
         rainHealth(),
         somsHealth(),
         piezoHealth()
     ];
 
-    sensor_overview.forEach((data) => {
-        data.done((value) => {
+    const deactivated = [
+        deactivatedLoggers(),
+        deactivatedSensors()
+    ];
+
+    $.when(...deactivated).then((...args) => {
+        args.forEach((obj) => {
+            obj[0].forEach((info) => {
+                createTables(info[0], info[1], info[2], info[3]);
+            });
+        });
+    });
+
+    active_loggers.forEach((data) => {
+        data.then((value) => {
             value.forEach((val) => {
-                createTables(val[0], val[1]);
+                createTables(val[0], val[1], val[2], val[3]);
             });
         });
     });
 }
 
-function createTables (data_source, table_id) {
-    const table_container = $("#table-container").clone().removeAttr("id").prop("hidden", false);
-    const table = $("#table-div").clone();
-    const table_template = table.find("#table-template").clone().prop("hidden", false);
+function createTables (data_source, tab_id, title, table_title) {
+    switch (table_title) {
+        case "#deact-sensor-div":
+            table_container = $("#deact-table-container")
+            .removeAttr("id")
+            .prop("hidden", false);
+            table = $(`${table_title}`);
+            table_template = table
+            .find("#deact-sensor-template")
+            .clone()
+            .prop("hidden", false);
+            break;
+        case "#deact-logger-div":
+            table_container = $("#deact-table-container")
+            .removeAttr("id")
+            .prop("hidden", false);
+            table = $(`${table_title}`);
+            table_template = table
+            .find("#deact-logger-template")
+            .clone()
+            .prop("hidden", false);
+            break;
+        default:
+            table_container = $("#table-container")
+            .clone()
+            .removeAttr("id")
+            .prop("hidden", false);
+            table = $("#table-div").clone();
+            table_template = table
+            .find("#table-template")
+            .clone()
+            .prop("hidden", false);
+            break;
+    }
 
-    if (table_id === "#deact-sensor" || table_id === "#deact-logger") {
+    if (table_title) {
         data_source.forEach((info) => {
-            table_template.find(".removable").remove();
-            table_template.find("#cheader").text("Date Deactivated");
-
             const table_row = table_template
             .find("#deact-row-template")
             .clone()
             .removeAttr("id")
             .prop("hidden", false);
 
-            table_row.find("#logger-name").text(info.logger_name);
-            table_row.find("#date-deactivated").text(info.date_deactivated);
+            table.find("#table-title").text(title).prop("hidden", false);
+            table_row.find("#logger-name").text(info.logger_name.toUpperCase());
+            table_row.find("#date-deactivated").text(moment(info.date_deactivated).format("DD MMMM YYYY HH:mm"));
             table_template.append(table_row);
         });
     } else {
         data_source.forEach((info) => {
-            const table_row = table_template.find("#row-template").clone().removeAttr("id").prop("hidden", false);
             const {
                 logger_name, time_delta, data_presence, last_data
             } = info;
+
             const lookup = [
-                ["#logger-name", logger_name],
+                ["#logger-name", logger_name.toUpperCase()],
                 ["#time-delta", time_delta],
                 ["#data-presence", data_presence],
-                ["#last-data-available", last_data]
+                ["#last-data-available", moment(last_data).format("DD MMMM YYYY HH:mm")]
             ];
+
+            const table_row = table_template
+            .find("#row-template")
+            .clone()
+            .removeAttr("id")
+            .prop("hidden", false);
+
             lookup.forEach(([element, text]) => {
                 table_row.find(element).text(text);
             });
@@ -104,6 +154,6 @@ function createTables (data_source, table_id) {
     }
     table.append(table_template);
     table_container.append(table);
-    $(`${table_id}`).append(table_container);
+    $(`${tab_id}`).append(table_container);
     table_template.DataTable();
 }
